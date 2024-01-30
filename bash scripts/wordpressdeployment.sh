995bfa7275013e7f711c87396f050cfc7afbdf7c
ahmd7 <ahmed.s.abdelrahim7@gmail.com>
2024-01-30 20:40:07 +0200

    edited

--- bash scripts/wordpressdeployment.sh
+++ bash scripts/wordpressdeployment.sh
@@ -3,17 +3,17 @@ sudo apt update -y
 sudo apt install apache2 php wget unzip php-mysql php-cgi php-cli php-gd -y
 sudo wget https://wordpress.org/latest.zip
 sudo unzip latest.zip
-sudo cd wordpress/
+cd wordpress
 sudo rm -rf /var/www/html
 sudo sudo mkdir /var/www/html
 sudo cp -r * /var/www/html
-sudo cd /var/www/html/
+cd /var/www/html/
 sudo rm -rf index.html
 sudo systemctl restart apache2
 sudo chown -R www-data:www-data /var/www
 sudo cp wp-config-sample.php wp-config.php
 #change the parameters
-sudo apt install mariadb-client mysql-server libapache2-mod-php -y
+sudo apt install mysql-server libapache2-mod-php -y
 sudo systemctl restart apache2
 #mysql
 #Database commands
